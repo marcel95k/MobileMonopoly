0be3b4e7cf6ea3587491d97776e36163e28f51e4
marcel.kiehtreiber1995@outlook.de <marcel.kiehtreiber1995@outlook.de>
2021-10-05 21:18:50 +0200

    a

--- MobileMonopoly/classPlayer.h
+++ MobileMonopoly/classPlayer.h
@@ -21,7 +21,7 @@ private:
 	bool isLendingMoney = false;	/*Checks if the a player is currently lending money for the bank.*/
 public:
 
-classPlayer::classPlayer(string nameValue, int moneyValue) {			/*Constructor of classPlayer.*/
+ classPlayer(string nameValue, int moneyValue) {			/*Constructor of classPlayer.*/
 	playerName = nameValue;												/*Hands over the value for playerName and playerMoney*/
 	playerMoney = moneyValue;
 	myGroupStrings->resize(9);
@@ -30,22 +30,22 @@ classPlayer::classPlayer(string nameValue, int moneyValue) {			/*Constructor of 
 
 /*Calling Set-Methods*/
 
-void classPlayer::setPlayerName(string value) {		/*Applies the handed over value to playerName.*/
+void  setPlayerName(string value) {		/*Applies the handed over value to playerName.*/
 	playerName = value;
 }
-void classPlayer::setPlayerMoney(int value) {		/*Applies the handed over value to playerMoney.*/
+void  setPlayerMoney(int value) {		/*Applies the handed over value to playerMoney.*/
 	playerMoney = value;
 }
-void classPlayer::losePlayerMoney(int value) {		/*Reduces the amount of playerMoney by the handed over value.*/
+void  losePlayerMoney(int value) {		/*Reduces the amount of playerMoney by the handed over value.*/
 	playerMoney -= value;
 }
-void classPlayer::addPlayerMoney(int value) {		/*Increases the amount of playerMoney by the handed over value.*/
+void  addPlayerMoney(int value) {		/*Increases the amount of playerMoney by the handed over value.*/
 	playerMoney += value;
 }
-void classPlayer::goBackOnField(int value) {		/*Used to let the player walk in the opposite direction on the field.*/
+void  goBackOnField(int value) {		/*Used to let the player walk in the opposite direction on the field.*/
 	currentPosition -= value;
 }
-void classPlayer::setNewPosition(int value) {		/*Sets the new position of the player on the gamefield by adding the value of the dice to the current position.*/
+void  setNewPosition(int value) {		/*Sets the new position of the player on the gamefield by adding the value of the dice to the current position.*/
 	int difference = 0;								/*In case the current position plus the value of the dice exceeds the amount of 39,*/
 	int diffToGo;									/*the difference is calculated and the player starts again from field 0.*/
 	if (currentPosition + value == 40) {			/*2000 is added to the variable playerMoney.*/
@@ -64,47 +64,47 @@ void classPlayer::setNewPosition(int value) {		/*Sets the new position of the pl
 	}
 	else currentPosition += value;
 }
-void classPlayer::addToMyGroupStrings(string fieldName, int value) {			/*Change the string of myGroupStrings to the handed over value at the handed over position.*/
+void  addToMyGroupStrings(string fieldName, int value) {			/*Change the string of myGroupStrings to the handed over value at the handed over position.*/
 	myGroupStrings->at(value) = myGroupStrings->at(value) + fieldName + ", ";
 }
-void classPlayer::addToMyGroup(int value, int position) {			/*Increases the value of the position in myGroup based on the handed over value.*/
+void  addToMyGroup(int value, int position) {			/*Increases the value of the position in myGroup based on the handed over value.*/
 	myGroup->at(position) += value;
 }
-bool classPlayer::checkforFullGroup(int position, string fieldTypeValue) { /*Check for full group by getting the fieldtype of the field and comparing it with the handed over value.*/
+bool  checkforFullGroup(int position, string fieldTypeValue) { /*Check for full group by getting the fieldtype of the field and comparing it with the handed over value.*/
 
 	if (fieldTypeValue == "2group" && myGroup->at(position) == 2 || fieldTypeValue == "3group" && myGroup->at(position) == 3) {
 		return true;
 	}
 	else { return false; }
 }
-void classPlayer::addMyField(int value) {			/*Pushes the handed over value to the vector myField.*/
+void  addMyField(int value) {			/*Pushes the handed over value to the vector myField.*/
 	myField->push_back(value);
 }
-void classPlayer::setIsMyField(bool value) {		/*Switches isMyField between true and false, depending on the handed over value.*/
+void  setIsMyField(bool value) {		/*Switches isMyField between true and false, depending on the handed over value.*/
 	isMyField = value;
 }
-void classPlayer::setIsInJail(bool value) {			/*Switches IsInJail between true and false, depending on the handed over value.*/
+void  setIsInJail(bool value) {			/*Switches IsInJail between true and false, depending on the handed over value.*/
 	IsInJail = value;
 }
 
 /*Calling Get-Methods*/
 
-string classPlayer::getPlayerName() { return playerName; }
-int classPlayer::getPlayerMoney() { return playerMoney; }
-int classPlayer::getAmountOfMyFields() { return amountOfMyFields; }
-int classPlayer::getCurrentPosition() { return currentPosition; }
-void classPlayer::getMyGroupStrings() {
+string  getPlayerName() { return playerName; }
+int  getPlayerMoney() { return playerMoney; }
+int  getAmountOfMyFields() { return amountOfMyFields; }
+int  getCurrentPosition() { return currentPosition; }
+void  getMyGroupStrings() {
 	cout << "\n";
 	for (int x = 0; x < myGroupStrings->size(); x++) {
 		cout << "\tGroup " << x << ": " << myGroupStrings->at(x) << "\n";
 	}
 }
-void classPlayer::getMyField() {
+void  getMyField() {
 	for (int x = 0; x < myField->size(); x++) {
 		cout << myField->at(x) << " ";
 	}
 }
-bool classPlayer::getIsMyField() { return isMyField; }
-bool classPlayer::getIsInJail() { return IsInJail; }
-bool classPlayer::getIsLendingMoney() { return isLendingMoney; }
+bool  getIsMyField() { return isMyField; }
+bool  getIsInJail() { return IsInJail; }
+bool  getIsLendingMoney() { return isLendingMoney; }
 };
